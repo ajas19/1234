5cc22b66ec80b37cb2630896bb4a8e219ef5699e
javateacher0517 <143336720+javateacher0517@users.noreply.github.com>
2023-08-27 23:08:58 +0530

    Update Question_01.javaa

--- Question_01.java
+++ Question_01.java
@@ -1,135 +1,157 @@
-upackage LinkDemo;
+package Array;
 
-public class LinkList1 {
-    Node head;
+import java.util.Scanner;
 
+import static java.lang.System.exit;
+import static java.lang.System.in;
 
-    public void insertFront(int data) {
-
-        Node newNode = new Node(data);
-
-        if (isEmpty())
+public class ArrayStructure {
+    public static void insert(int arr[],int value,int index)
+    {
+        if (index < arr.length && index > 0)
         {
-            head=newNode;
-            return;
-        }
-        newNode.next=head;
-        head = newNode;
-
-
-    }
-
-
-    public void insertAfter(int data,int pos) {
-
-        Node newNode = new Node(data);
-
-        if (isEmpty())
-        {
-            System.out.println("List is Empty");
-            return;
-        }
-
-        Node current=head;
-        while (current!=null)
-        {
-            if (current.data==pos)
+            for (int i= arr.length-1; i>index;i--)
             {
-                newNode.next=current.next;
-                current.next=newNode;
+                arr[i] = arr[i-1];
             }
-            current=current.next;
+            arr[index] = value;
+
         }
 
     }
 
-    public void insertEnd(int data) {
-        Node newNode = new Node(data);
 
-        if (isEmpty())
+    public static void search(int arr[],int value)
+    {
+        int key =1;
+
+        for (int i=0;i< arr.length-1;i++)
         {
-            head=newNode;
-            return;
-        }
-
-        Node last= head;
-
-        while (last.next!=null)
-        {
-            last= last.next;
-        }
-
-        last.next=newNode;
-        newNode=last;
-
-    }
-
-
-    public void search(int key) {
-
-        int test=1;
-
-        if (isEmpty())
-        {
-            System.out.println("list is Empty");
-            return;
-        }
-
-        Node current= head;
-
-        while (current!=null)
-        {
-            if (current.data==key)
+            if (arr[i] == value)
             {
-                System.out.println("Value is found");
+                System.out.println("Value is found in"+i+" index");
                 key=1;
                 break;
             }else
             {
                 key=0;
             }
-            current=current.next;
         }
         if (key==0)
         {
-            System.out.println("not found");
-        }
-
-    }
-
-
-    public void display() {
-
-        if (isEmpty())
-        {
-            System.out.println("List is Empty");
-            return;
-        }
-
-        Node current=head;
-        while (current!=null)
-        {
-            System.out.println(current.data);
-            current=current.next;
+            System.out.println("Value is not found");
         }
     }
 
-    public boolean isEmpty() {
-        return head==null;
+
+    public static void delete(int arr[],int value)
+    {
+        int key=1;
+
+        for (int i=0;i<arr.length;i++)
+        {
+            if (arr[i] == value)
+            {
+                System.out.println("value is "+ i + "Student");
+
+                for (int j=i;j<arr.length-1;j++)
+                {
+                    arr[j] = arr[i+1];
+                }
+                arr[arr.length-1] =0;
+
+                key=1;
+                break;
+            }
+            else {
+                key=0;
+            }
+        }
+
+        if (key==0)
+        {
+            System.out.println("that value value is not exist");
+        }
+    }
+
+
+
+
+
+    public static void display(int arr[])
+    {
+
+        for (int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 
 
     public static void main(String[] args) {
 
-        LinkList1 ls = new LinkList1();
-        ls.insertFront(10);
-        ls.insertEnd(50);
-        ls.insertFront(20);
-        ls.insertAfter(450,50);
-        ls.search(25);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int size = input.nextInt();
+        System.out.println("");
 
-        ls.display();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the " + (i + 1) + "value:");
+            int val = input.nextInt();
+            arr[i] = val;
+        }
 
 
+        while (true) {
+            System.out.println("");
+            System.out.println("1)insert number");
+            System.out.println("2)Delete number");
+            System.out.println("3)search");
+            System.out.println("4)display");
+            System.out.println("5)Exit");
+            System.out.println("Select the number from above option");
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+
+                    System.out.println("Enter the value need to be store:");
+                    int val= input.nextInt();
+                    System.out.println("Enter the index need to be store:");
+                    int index= input.nextInt();
+                    insert(arr,val,index);
+                    display(arr);
+                    break;
+                case 2:
+
+                    System.out.println("Enter the value need to be delete:");
+                    int del= input.nextInt();
+                    delete(arr,del);
+                    display(arr);
+
+                    break;
+                case 3:
+                    System.out.println("Enter the value need to be search:");
+                    int sear= input.nextInt();
+                    search(arr,sear);
+                    break;
+                case 4:
+                    display(arr);
+                    break;
+                case 5:
+                    exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+
+
+            }
+
+        }
     }
+
+
 }
